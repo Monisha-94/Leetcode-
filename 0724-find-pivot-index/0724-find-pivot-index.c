494f714491161589452dd89dd7ea8875f9ea3d22
Monisha-94 <monishakolipaka07@gmail.com>
2026-08-25 23:59:58 -0700

    Time: 0 ms (100%), Space: 10.2 MB (72.13%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -0,0 +1,15 @@
+int pivotIndex(int* nums, int n) {
+    int totalsum=0;
+    for (int i=0;i<n;i++){
+        totalsum+=nums[i];
+    }
+    int leftsum=0;
+    for(int i=0;i<n;i++){
+      int  rightsum=totalsum-leftsum-nums[i];
+        if(leftsum==rightsum)
+        return i;
+        leftsum+=nums[i];
+    }
+    return -1;
+}
+    
